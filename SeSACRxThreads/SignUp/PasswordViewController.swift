7c7993f6711c3f761dac05aff6e31782f92e2658
chaewon-moana <dreamytaurus508@gmail.com>
2024-04-01 20:45:02 +0900

    PhoneViewController 조건 2번 필수요건 처리 완료

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
@@ -14,23 +16,48 @@ class PasswordViewController: UIViewController {
     let nextButton = PointButton(title: "다음")
     let descriptionLabel = UILabel()
     
+    let validText = Observable.just("8자 이상 입력해주세요")
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = Color.white
-        
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+    func bind() {
+        validText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let validation = passwordTextField.rx.text.orEmpty
+            .map { $0.count >= 8 }
+        
+        validation
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .systemPink : .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let vc = PhoneViewController()
+                owner.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
     func configureLayout() {
         view.addSubview(passwordTextField)
         view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
          
         passwordTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
@@ -41,6 +68,12 @@ class PasswordViewController: UIViewController {
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(passwordTextField.snp.bottom)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,21 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let descLabel = UILabel()
+    
+    var defaultPhoneNumber = "010"
+    
+    lazy var phoneNumber = BehaviorSubject(value: defaultPhoneNumber)
+    let validText = Observable.just("올바른 전화번호를 입력해주세요")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,28 +29,65 @@ class PhoneViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
+        bind()
         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
+    func bind() {
+        validText
+            .bind(to: descLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneNumber
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .map { Int($0) ?? 0 }
+            .subscribe(with: self) { owner, value in
+                owner.defaultPhoneNumber = "0" + String(value)
+                owner.phoneNumber.onNext(owner.defaultPhoneNumber)
+            }
+            .disposed(by: disposeBag)
+        let textCount = phoneTextField.rx.text.orEmpty
+            .map { $0.count >= 10 } //10자 이상일 때 - true
+        
+        textCount
+            .bind(to: descLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        textCount
+            .bind(with: self) { owner, value in
+                owner.nextButton.backgroundColor = value ? .systemPink : .lightGray
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let vc = NicknameViewController()
+                owner.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
-
     
     func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
+        view.addSubview(descLabel)
          
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
-        
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        descLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(phoneTextField.snp.bottom)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
